fbd6d82c5ae3d41847ac86512a97e9cc07c5bda3
SebbyG <seb.hop@hotmail.com>
2016-04-26 15:50:58 +0200

    Lavet fruit klasse

---
+++ UDP/Server/FruitData.java
@@ -0,0 +1,25 @@
+package Server;
+
+import java.util.ArrayList;
+
+public class FruitData {
+	ArrayList<String> fruitArray = new ArrayList<String>();
+	
+	public FruitData(){
+		this.fruitArray.add("Æble");
+		this.fruitArray.add("Avokado");
+		this.fruitArray.add("Hindbær");
+		this.fruitArray.add("Blomme");
+		this.fruitArray.add("Banan");
+		this.fruitArray.add("Blåbær");
+		this.fruitArray.add("Vandmelon");
+		this.fruitArray.add("Jordbær");
+		this.fruitArray.add("Brombær");
+		this.fruitArray.add("Citron");
+		this.fruitArray.add("Honningmelon");
+	}
+	
+	public String getFruit(int ID){
+		return fruitArray.get(ID);
+	}
+}
